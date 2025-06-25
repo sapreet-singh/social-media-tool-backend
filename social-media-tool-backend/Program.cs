44ad9911abdfc58b457f67efddfc84ed634d10f5
Vibhu Babbar <vibhu.babbar@enercross.com>
2025-06-25 23:00:16 +0530

    created webhook controller  for Receive message and Send auto reply

--- social-media-tool-backend/Program.cs
+++ social-media-tool-backend/Program.cs
@@ -7,6 +7,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +29,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowAngularApp");
 app.UseAuthorization();
 
 app.MapControllers();
